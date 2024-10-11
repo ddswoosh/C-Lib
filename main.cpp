af0ccd467be733a9fd9bb7468cebbb0dc273bc80
ddswoosh <ddswoosh@gmail.com>
2024-10-10 18:11:10 -0700

    ln

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 // Header includes
-#include "Structs\Vectors\vector.cpp"
+#include "Structs/Vectors/vector.cpp"
 
 
 int main() {
@@ -14,7 +14,7 @@ int main() {
     vec->Append(343);
     vec->Append(-123); 
     std::cout << vec->Length();
-
+    //
 
     // for (int i =0; i < vec->Length(); ++i) {
      
